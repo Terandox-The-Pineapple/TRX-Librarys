fcce5363a8db7058c0ba743efb459d1c2830eb73
Terandox <rene-gilbert99@web.de>
2022-05-11 09:53:32 +0200

    Same

--- class.lua
+++ class.lua
@@ -1,10 +1,10 @@
 function new(value)
     local class = value or {}
 
-    function class.new(my_value)
-        local self = my_value or {}
-        setmetatable(self, { _index = class })
-        return self
+    function class:new(my_value)
+        local new_self = my_value or {}
+        setmetatable(new_self, { _index = self })
+        return new_self
     end
 
     function class:destroy()
