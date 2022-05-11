3adc2db9c38201c95f9480ede17f6bec68c932ae
Terandox <rene-gilbert99@web.de>
2022-05-11 17:51:54 +0200

    Fixed controller
    Fixed wrong code grama in controller class

--- game-utils.lua
+++ game-utils.lua
@@ -179,21 +179,22 @@ end
 
 menu = class_lib.class:new(menu)
 
-local controller = { buffer = false, finished = false, keys = {} }
+local controller = { finished = false, keys = {} }
 
 function controller:start(p_func)
     self.finished = false
-    self.buffer = false
     while self.finished == false do
         parallel.waitForAny(self.get_key, p_func)
     end
 end
 
-function controller.get_key()
-    local _, key = os.pullEvent("key")
-    for index, data in pairs(self.keys) do
-        if data.key == key then
-            data.func()
+function controller:get_key()
+    while self.finished == false do
+        local _, key = os.pullEvent("key")
+        for index, data in pairs(self.keys) do
+            if data.key == key then
+                data.func()
+            end
         end
     end
 end
